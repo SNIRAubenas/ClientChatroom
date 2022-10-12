275cd96f688e5372904f0eb9f2e0fd0300f1a35e
jfogeron <jules.fogeron@lyceeastier.com>
2022-10-12 11:42:30 +0200

    cree serveur

---
+++ SeurveurChatroom/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeurveurChatroom
+{
+    internal class Client
+    {
+        public Client(TcpClient tcpClient)
+        {
+            Thread thread = new Thread(() => run(tcpClient));
+            thread.Start();
+        }
+        public void run(TcpClient tcpClient)
+        {
+            NetworkStream stream = tcpClient.GetStream();
+            byte[] buffer = new byte[1024];
+            do
+            {
+                try
+                {
+
+                    int lu = stream.Read(buffer, 0, buffer.Length);
+                    if (lu == 0)
+                    {
+
+                        break;
+                    }
+                    string message = ASCIIEncoding.ASCII.GetString(buffer, 0, lu);
+                    Console.WriteLine(message);
+                    stream.Write(buffer, 0, lu);
+
+                }
+                catch
+                {
+                    break;
+                }
+
+            } while (true);
+            tcpClient.Close();
+
+        }
+
+
+    }
+}
---
+++ SeurveurChatroom/communication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeurveurChatroom
+{
+    internal class communication
+    {
+        private Form1 form1;
+
+        public communication(Form1 form1)
+        {
+            this.form1 = form1;
+
+        }
+        public void conection()
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+            int port = 18;
+            TcpListener listener = new TcpListener(iPAddress, port);
+            listener.Start();
+            do
+            {
+
+                TcpClient tcpClient = listener.AcceptTcpClient();
+                Client client = new Client(tcpClient);
+
+            } while (true);
+
+        }
+
+    }
+}
